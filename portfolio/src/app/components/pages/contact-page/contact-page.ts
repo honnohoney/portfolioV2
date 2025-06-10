3a777e2b69f6b00da3d4650bd7a43b1fa077bf98
honnohoney <irish254722@gmail.com>
2025-06-10 16:57:20 +0700

    adding contact-page data

--- portfolio/src/app/components/pages/contact-page/contact-page.ts
+++ portfolio/src/app/components/pages/contact-page/contact-page.ts
@@ -1,39 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact-page.html',
   styleUrl: './contact-page.scss',
 })
 export class ContactPage {
   mainContacts = [
-    {
-      type: 'GITHUB',
-      url: 'https://github.com/honnohoney',
-    },
-    {
-      type: 'FACEBOOK',
-      url: 'https://www.facebook.com/profile.php?id=100092000528523',
-    },
-    {
-      type: 'INSTAGRAM',
-      url: 'https://www.instagram.com/honnohoney/',
-    },
+    { type: 'GITHUB', url: 'https://github.com/honnohoney' },
+    { type: 'FACEBOOK', url: 'https://www.facebook.com/profile.php?id=100092000528523' },
+    { type: 'INSTAGRAM', url: 'https://www.instagram.com/honnohoney/' },
   ];
 
   contacts = [
-    {
-      type: 'MAIL',
-      text: 'hanuera.khim@gmail.com',
-    },
-    {
-      type: 'PHONE',
-      text: '+66997620997',
-    },
-    {
-      type: 'LOCATION',
-      text: 'Loei, Thailand',
-    },
+    { type: 'MAIL', text: 'hanuera.khim@gmail.com' },
+    { type: 'PHONE', text: '+66997620997' },
+    { type: 'LOCATION', text: 'Loei, Thailand' },
   ];
+
+ copiedMessage = {
+    x: 0,
+    y: 0,
+    show: false,
+  };
+
+  goToPage(url: string) {
+    window.open(url, '_blank');
+  }
+
+  async copyToClipboard(event: MouseEvent, text: string) {
+    try {
+      await navigator.clipboard.writeText(text);
+      this.copiedMessage = {
+        x: event.clientX,
+        y: event.clientY,
+        show: true,
+      };
+      setTimeout(() => {
+        this.copiedMessage.show = false;
+      }, 1200);
+    } catch (err) {
+      console.error('Clipboard copy failed:', err);
+    }
+  }
 }
