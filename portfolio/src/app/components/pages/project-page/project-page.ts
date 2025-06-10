4de7d1caa38a69fe46466414f6ef20fc019c3e9f
honnohoney <irish254722@gmail.com>
2025-06-10 15:20:53 +0700

    adding mat menu

--- portfolio/src/app/components/pages/project-page/project-page.ts
+++ portfolio/src/app/components/pages/project-page/project-page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { ProjectCard } from '../../shared/project-card/project-card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-project-page',
-  imports: [ProjectCard],
+  imports: [ProjectCard, MatMenuModule],
   templateUrl: './project-page.html',
-  styleUrl: './project-page.scss'
+  styleUrl: './project-page.scss',
 })
-export class ProjectPage {
-
-}
+export class ProjectPage {}
