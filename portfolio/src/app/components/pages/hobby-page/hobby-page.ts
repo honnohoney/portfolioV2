7a335d159271bbb6523a2741a5cfe1feda5ae149
honnohoney <irish254722@gmail.com>
2025-06-11 14:45:29 +0700

    adding hobby and project cards

--- portfolio/src/app/components/pages/hobby-page/hobby-page.ts
+++ portfolio/src/app/components/pages/hobby-page/hobby-page.ts
@@ -5,8 +5,24 @@ import { HobbyCard } from '../../shared/hobby-card/hobby-card';
   selector: 'app-hobby-page',
   imports: [HobbyCard],
   templateUrl: './hobby-page.html',
-  styleUrl: './hobby-page.scss'
+  styleUrl: './hobby-page.scss',
 })
 export class HobbyPage {
-
+  hobbyList = [
+    {
+      title: 'Crochet',
+      desc: 'i’ve crocheting some cute stuff, bag, cute gloves etc...',
+      image: '',
+    },
+    {
+      title: 'Reading',
+      desc: 'i enjoy reading philosophy, psychology, horror, thriller',
+      image: '',
+    },
+    {
+      title: 'Gaming',
+      desc: 'i enjoy playing online game like fps, action-survival horror game',
+      image: '',
+    },
+  ];
 }
--- portfolio/src/app/components/pages/project-page/project-page.ts
+++ portfolio/src/app/components/pages/project-page/project-page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProjectCard } from '../../shared/project-card/project-card';
 import { MatMenuModule } from '@angular/material/menu';
+import { TagType, TagDisplayText } from '../../shared/models/project-tag.model';
 
 @Component({
   selector: 'app-project-page',
@@ -8,4 +9,50 @@ import { MatMenuModule } from '@angular/material/menu';
   templateUrl: './project-page.html',
   styleUrl: './project-page.scss',
 })
-export class ProjectPage {}
+export class ProjectPage {
+  selectedOption = 0;
+
+  projectList = [
+    {
+      title: 'Frontend dev project',
+      desc: ' Frontend project i did when i was an employee as Frontend developer',
+      image: '',
+      tag: [TagType.WEBSITE],
+    },
+    {
+      title: '3D Models sculpting',
+      desc: "3D model I'm currently practicing",
+      image: '',
+      tag: [TagType.THREE_D],
+    },
+    {
+      title: '2D drawing',
+      desc: ' My drawing project',
+      image: '',
+      tag: [TagType.DRAWING],
+    },
+  ];
+
+  menuOptions = [
+    {
+      name: TagDisplayText[TagType.ALL],
+      type: TagType.ALL,
+    },
+    {
+      name: TagDisplayText[TagType.WEBSITE],
+      type: TagType.WEBSITE,
+    },
+    {
+      name: TagDisplayText[TagType.THREE_D],
+      type: TagType.THREE_D,
+    },
+    {
+      name: TagDisplayText[TagType.DRAWING],
+      type: TagType.DRAWING,
+    },
+  ];
+
+  onSelectOption(index: number) {
+    this.selectedOption = index;
+  }
+}
