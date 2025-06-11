3a1d2f5c1c8bd6e348794a60e6f13245e5b03e82
honnohoney <irish254722@gmail.com>
2025-06-11 14:46:37 +0700

    show card detail from input

--- portfolio/src/app/components/shared/hobby-card/hobby-card.ts
+++ portfolio/src/app/components/shared/hobby-card/hobby-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hobby-card',
   imports: [],
   templateUrl: './hobby-card.html',
-  styleUrl: './hobby-card.scss'
+  styleUrl: './hobby-card.scss',
 })
 export class HobbyCard {
-
+  @Input() hobby: { title: string; desc: string; image: string } = {
+    title: '',
+    desc: '',
+    image: '',
+  };
 }
--- portfolio/src/app/components/shared/project-card/project-card.ts
+++ portfolio/src/app/components/shared/project-card/project-card.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TagType } from '../models/project-tag.model';
 
 @Component({
   selector: 'app-project-card',
   imports: [],
   templateUrl: './project-card.html',
-  styleUrl: './project-card.scss'
+  styleUrl: './project-card.scss',
 })
 export class ProjectCard {
-
+  @Input() projectDetail: {
+    title: string;
+    desc: string;
+    image: string;
+    tag?: TagType[];
+  } = {
+    title: '',
+    desc: '',
+    image: '',
+  };
 }
